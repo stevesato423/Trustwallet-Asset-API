babce42ff9e4b446c79c91a4c8cd8c0b381e4afa
stevesato423 <stevesato423@gmail.com>
2021-09-13 01:06:20 +0200

    fix(symbol): re-order matching

--- api/symbol/[token].ts
+++ api/symbol/[token].ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import axios from 'axios';
-import { uniqBy, flatten } from 'lodash';
+import { uniqBy, flatten, orderBy } from 'lodash';
 import { map } from 'p-iteration';
 import request from 'request';
 
@@ -10,9 +10,10 @@ const TOKENS_LIST = [
   'https://yearn.science/static/tokenlist.json',
   'https://gateway.ipfs.io/ipns/tokens.uniswap.org',
   'https://unpkg.com/@sushiswap/default-token-list/build/sushiswap-default.tokenlist.json',
-  'https://unpkg.com/quickswap-default-token-list/build/quickswap-default.tokenlist.json',
   'https://unpkg.com/@cometh-game/default-token-list/build/comethswap-default.tokenlist.json',
   'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json',
+  'https://unpkg.com/default-token-list@2.27.3/build/tokens.json',
+  'https://unpkg.com/quickswap-default-token-list/build/quickswap-default.tokenlist.json',
 ];
 
 const bscTokensJson =
@@ -26,7 +27,7 @@ const tokenRewrites: Record<string, string> = {
   BTC: 'btcb',
   BNB: 'wbnb',
   pAUTO: 'AUTO',
-  QUICK: 'Quick',
+  dQUICK: 'QUICK',
 };
 
 const customImages: Record<string, string> = {
@@ -36,6 +37,8 @@ const customImages: Record<string, string> = {
   CIFI: 'https://superlauncher.io/img/project/cifi/cifi-logo.svg',
   BYG: 'https://superlauncher.io/img/project/black-eye-galaxy-logo.png',
   C98: 'https://assets.trustwalletapp.com/blockchains/smartchain/assets/0xaEC945e04baF28b135Fa7c640f624f8D90F1C3a6/logo.png',
+  QUICK:
+    'https://raw.githubusercontent.com/trustwallet/assets/6572a691df8141f3e8213cb97ef8d2e4b86d8b86/blockchains/ethereum/assets/0x6c28AeF8977c9B773996d0e8376d2EE379446F2f/logo.png',
 };
 
 interface Token {
@@ -61,10 +64,19 @@ async function fetchTokensLists(): Promise<Token[]> {
   const tokens = await map(TOKENS_LIST, async (url) => {
     const { data } = await axios.get<{ tokens: Token[] }>(url);
     return Array.isArray(data?.tokens)
-      ? data.tokens.filter((t) => t.chainId === 137)
+      ? orderBy(
+          data.tokens.filter(
+            (t) => t.chainId === 137 || t.chainId === 1 || t.chainId === 56
+          ),
+          ['chainId'],
+          ['asc']
+        )
       : [];
   });
-  return uniqBy(flatten(tokens), 'address');
+  return uniqBy(
+    flatten(tokens),
+    (token) => `${token.chainId}-${token.address}`
+  );
 }
 
 async function searchTokenInLists(
@@ -77,44 +89,34 @@ async function searchTokenInLists(
 async function searchToken(
   tokenSymbol: string
 ): Promise<{ logoURI: string } | null> {
-  const _tokenSymbol = tokenRewrites[tokenSymbol] || tokenSymbol;
-
-  if (customImages[_tokenSymbol]) {
-    return { logoURI: customImages[_tokenSymbol] };
-  }
-
-  const listMatch = await searchTokenInLists(_tokenSymbol);
-
-  if (listMatch) {
-    return listMatch;
-  }
-
-  const {
-    data: { tokens: bscTokens },
-  } = await axios.get<{ tokens: TrustWalletAsset[] }>(bscTokensJson);
-
-  const bscMatch = bscTokens.find(
-    (t) =>
-      t.symbol === _tokenSymbol ||
-      t.symbol.toLowerCase() === _tokenSymbol.toLowerCase()
-  );
-
-  if (bscMatch) {
-    return bscMatch;
+  if (customImages[tokenSymbol]) {
+    return { logoURI: customImages[tokenSymbol] };
   }
 
   const {
     data: { tokens: ethTokens },
   } = await axios.get<{ tokens: TrustWalletAsset[] }>(ethTokensJson);
 
-  const ethMatch = ethTokens.find(
-    (t) =>
-      t.symbol === _tokenSymbol ||
-      t.symbol.toLowerCase() === _tokenSymbol.toLowerCase()
-  );
+  const ethMatch = ethTokens.find((t) => t.symbol === tokenSymbol);
 
   if (ethMatch) {
     return ethMatch;
+  }
+
+  const {
+    data: { tokens: bscTokens },
+  } = await axios.get<{ tokens: TrustWalletAsset[] }>(bscTokensJson);
+
+  const bscMatch = bscTokens.find((t) => t.symbol === tokenSymbol);
+
+  if (bscMatch) {
+    return bscMatch;
+  }
+
+  const listMatch = await searchTokenInLists(tokenSymbol);
+
+  if (listMatch) {
+    return listMatch;
   }
 
   return null;
@@ -124,7 +126,8 @@ async function handler(req: VercelRequest, res: VercelResponse): Promise<any> {
   // cache response one year
   res.setHeader('Cache-Control', 's-maxage=360000, stale-while-revalidate');
 
-  const tokenSymbol = req.query.token as string;
+  const tokenSymbolQuery = req.query.token as string;
+  const tokenSymbol = tokenRewrites[tokenSymbolQuery] || tokenSymbolQuery;
   const token = await searchToken(tokenSymbol);
 
   if (token) {
